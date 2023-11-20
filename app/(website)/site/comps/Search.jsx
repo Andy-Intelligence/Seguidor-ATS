2f5b1ce9cff4312897ab4c7f1c0b61050d28b507
Andidiong <usoroandidiong@gmail.com>
2023-11-20 22:47:17 +0100

    made changes

--- app/(website)/site/comps/Search.jsx
+++ app/(website)/site/comps/Search.jsx
@@ -16,8 +16,8 @@ const Search = () => {
 
 
 
-    <>
-      <input
+    <div className="flex items-center justify-center">
+      {/* <input
         type="search"
         id="search"
         placeholder="Search..."
@@ -25,8 +25,25 @@ const Search = () => {
         className=" moverPlaceholder relative mt-3 ml-6"
       />
       <Image src={search} width={30} className="main-image" alt="search" height={100} quality={100} />
-      
-    </>
+       */}
+        <div className='relative'>
+          <div className='absolute left-[12px] top-[8px]'>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M11 20C15.9706 20 20 15.9706 20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20Z" stroke="#989898" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M18.9299 20.6898C19.4599 22.2898 20.6699 22.4498 21.5999 21.0498C22.4499 19.7698 21.8899 18.7198 20.3499 18.7198C19.2099 18.7098 18.5699 19.5998 18.9299 20.6898Z" stroke="#989898" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </div>
+          <input
+            type="text"
+            placeholder="I am looking for..."
+            className="navbarSearchInput rounded-[4px] p-2 w-[400px] h-[40px] block pl-12 font-[500] text-[14px]"
+            onChange={handleChange}
+          // value={searchQuery}
+          />
+           {/* <button onClick={() =>{onSearch && onSearch(searchQuery)}}>Search</button> */}
+          
+        </div>
+    </div>
   );
 };
 
