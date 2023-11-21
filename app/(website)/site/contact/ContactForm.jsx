c011f9eb3aadac06738df4054f353462f2209d92
Andidiong <usoroandidiong@gmail.com>
2023-11-21 08:38:28 +0100

    made changes

--- app/(website)/site/contact/ContactForm.jsx
+++ app/(website)/site/contact/ContactForm.jsx
@@ -1,12 +1,12 @@
 const ContactForm = () => {
   return (
-    <div className=" bg-white contactForm p-20">
-      <div>
+    <div className="w-full contactForm p-20">
+      <div className="flex flex-col gap-2 w-full">
         <h2 className=" text-black mb-6 text-center font-bold">Reach Out To Us</h2>
-        <input type="text" placeholder="Company Name" className=" border  border-black bg-white" />
-        <input type="email" placeholder="Company Email" className=" bg-white border  border-black" />
-        <textarea placeholder="Write Here" cols="44" rows="5" className=" border border-black p-9"></textarea>
-        <input type="submit" value="Send" className="contact text-white px-9" />
+        <input type="text" placeholder="Company Name" className=" border   bg-white" />
+        <input type="email" placeholder="Company Email" className=" bg-white border border-none p-9 " />
+        <textarea placeholder="Write Here" cols="44" rows="5" className="mb-4 border border-none w-full p-9"></textarea>
+        <input type="submit" value="Send" className="contact text-white px-9 w-full" />
       </div>
     </div>
   );
