7cd1c81bf7232af885bbf3d27ec932272a5f9117
Andidiong <usoroandidiong@gmail.com>
2023-11-21 08:18:43 +0100

    made changes

--- app/(website)/site/About/HeaderText.jsx
+++ app/(website)/site/About/HeaderText.jsx
@@ -1,8 +1,8 @@
 const HeaderText = () => {
     return (
         <div>
-            <h2 className=" text-center text-2xl">SEGUIDOR</h2>
-            <p className=" about-p">At Seguidor, we're on a mission to transform the way organizations approach HR management. With a dedicated team of experts and a passion for innovative solutions, we've <span className="block">been empowering businesses to make  the most valuable asset- their people.</span></p>
+            <h2 className=" text-center text-2xl font-[700] pt-4">Seguidor</h2>
+            <p className=" about-p text-md">At Seguidor, we're on a mission to transform the way organizations approach HR management. With a dedicated team of experts and a passion for innovative solutions, we've <span className="block">been empowering businesses to make  the most valuable asset- their people.</span></p>
         </div>
     );
 }
--- app/(website)/site/About/MainArea.jsx
+++ app/(website)/site/About/MainArea.jsx
@@ -5,7 +5,7 @@ const MainArea = () => {
     <div className=" mainArea ">
       <div className="detailsTwo">
       <h2 className=" font-bold text-2xl">Our Story</h2>
-      <p>(Your company name) was founded with a simple yet powerful goal: to simplify and improve HR<span className="block mt-1">
+      <p className="">Seguidor was founded with a simple yet powerful goal: to simplify and improve HR<span className="block mt-1">
         Processes for organization of all sizes. Our journey began when a group of HR professionals and tech
       </span><span className=" block mt-1">enthusiasts  came together to create a solution that would make HR management more efficient</span>
       <span className="block mt-1">effective, and user friendly.</span></p>
