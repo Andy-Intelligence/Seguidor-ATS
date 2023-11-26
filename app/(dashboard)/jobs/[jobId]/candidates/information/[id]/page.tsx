32afc2f980850abe3c3f2931337a41ad33f59e74
Andidiong <usoroandidiong@gmail.com>
2023-11-26 18:24:27 +0100

    made changes

--- app/(dashboard)/jobs/[jobId]/candidates/information/[id]/page.tsx
+++ app/(dashboard)/jobs/[jobId]/candidates/information/[id]/page.tsx
@@ -280,18 +280,9 @@ fetchData().then((a)=>{
       
     })
   
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">Form Submitted</code>
-        </pre>
-      ),
-    })
-  
+
     handleClose()
-  
-    console.log(startDateValue,endValue,values)
+
   }
 
 
