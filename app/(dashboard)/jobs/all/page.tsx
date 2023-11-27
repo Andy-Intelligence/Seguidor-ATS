cb6758780cd4a6c60ec642aceaab09495323b74b
Andidiong <usoroandidiong@gmail.com>
2023-11-27 18:31:46 +0100

    made changes

--- app/(dashboard)/jobs/all/page.tsx
+++ app/(dashboard)/jobs/all/page.tsx
@@ -129,9 +129,9 @@ export default function Home() {
 
       </div>
     </nav>
-    <div className='w-fulll flex items-center justify-center'>
+    <div className='w-full flex items-center justify-center'>
         {/* <main className="grid grid-cols-2 justify-between gap-10  p-2"> */}
-        <main className="grid grid-rows-2 justify-between justify-items-center grid-flow-col gap-4">
+        <main className="grid ww-full p-4 grid-cols-2 justify-evenly justify-items-center grid-flow-col gap-4">
         {filteredjobs?.map((item:any)=>{
                       return (
                         <div onClick={(e)=>handleClick(e,item?._id)}>
